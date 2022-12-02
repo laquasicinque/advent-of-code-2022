429cce2c8d62c2311df178d34b061db153fe3f19
Adam Dorling <adam.x.dorling@gmail.com>
2022-12-02 16:56:16 +0000

    :art: formatting

--- days/01/index.ts
+++ days/01/index.ts
@@ -1,16 +1,18 @@
 import { getInput } from "../_utils/getInput.ts";
 import { sum } from "../_utils/sum.ts";
 
-const input = getInput()
+const input = getInput();
 
 function highestCalories(str: string, count = 1): number {
-    const totalCals = str
-        .split('\n\n')
-        .map(
-            x => sum(x.split('\n'))
-        ).sort((a, b) => b - a)
-    return sum(totalCals.slice(0, count))
+  const totalCals = str
+    .split("\n\n")
+    .map((x) => sum(x.split("\n")))
+    .sort((a, b) => b - a);
+  return sum(totalCals.slice(0, count));
 }
 
-console.log("Highest Calorie from individual: ", highestCalories(input))
-console.log("Sum of Top 3 Highest Calorie Carrying Individuals: ", highestCalories(input, 3))
+console.log("Highest Calorie from individual: ", highestCalories(input));
+console.log(
+  "Sum of Top 3 Highest Calorie Carrying Individuals: ",
+  highestCalories(input, 3)
+);
